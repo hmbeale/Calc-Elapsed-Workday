6996f7e6b53e7fa68dc852675115265e471f159a
Holden Beale <itdbealh@aikencounty.net>
2019-01-29 15:09:12 -0500

    refactor for readability

--- cew.js
+++ cew.js
@@ -1,16 +1,22 @@
 //calculate time as a percentage
-let d = new Date();
-let h = d.getHours() -8;
-let m = d.getMinutes();
-let s = d.getSeconds();
-let ms = d.getMilliseconds();
-let elapsedMs = (h*3600000) + (m*60000) + (s*1000) + ms;
+const d = new Date();
+const h = d.getHours() - 8;
+const m = d.getMinutes();
+const s = d.getSeconds();
+const ms = d.getMilliseconds();
+let elapsedMs = (h * 3600000) + (m * 60000) + (s * 1000) + ms;
 
-if (elapsedMs >= 16200000 && elapsedMs<=19800000){
-  elapsedMs = 16200000;
+
+//lunchbreak in elapsed hours, converted to ms
+const lunchStart = 4.5 * 3600000;
+const lunchEnd = 5.5 * 3600000;
+
+//accounting for lunchbreak
+if (elapsedMs >= lunchStart && elapsedMs <= lunchEnd) {
+    elapsedMs = lunchStart;
 }
-if (elapsedMs>19800000){
-  elapsedMs -=3600000;
+if (elapsedMs > lunchEnd) {
+    elapsedMs -= 3600000;
 }
 
-console.log((Math.round((elapsedMs/288000) * 100) / 100) + "%");
+console.log((Math.round((elapsedMs / 288000) * 100) / 100) + "%");
