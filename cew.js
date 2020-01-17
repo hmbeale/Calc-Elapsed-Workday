80af11dcc20161f9410918c2f831db19cbb8c083
hmbeale <holden.beale@gmail.com>
2020-01-17 15:34:15 -0500

    DRY

--- cew.js
+++ cew.js
@@ -1,15 +1,11 @@
-
-//'January 17, 20 17:00:00 GMT-05:00'
-
-let dayStart = new Date();
+const dayStart = new Date();
 dayStart.setHours(8,0,0,0); //start of workday in local time
-const workStart = new Date('November 17, 17 08:00:00 GMT-05:00'); //first workday
 const periodStart = new Date('January 10, 20 08:00:00 GMT-05:00'); //since announce
+const workStart = new Date('November 17, 17 08:00:00 GMT-05:00'); //first workday
 
-let dayStop = new Date();
+const dayStop = new Date();
 dayStop.setHours(17,0,0,0); //end of workday in local time
-const workStop = new Date('January 31, 20, 05:00:00 GMT-05:00') //resignation
-
+const workStop = new Date('January 31, 20, 05:00:00 GMT-05:00'); //resignation
 
 //takes date objects
 //returns total time, elapsed time, remaining time
@@ -17,66 +13,45 @@ const genStats = (start, stop) => {
     let res = [];
     startMs = start.getTime();
 
-    res[0] = stop.getTime() - startMs; //total time
-    res[1] = Date.now() - startMs; //elapsed time
-    res[2] = res[0] - res[1]; //remaining time 
-    return res;
+    const timeTotal = stop.getTime() - startMs; //total time
+    const timeElapsed = Date.now() - startMs; //elapsed time
+    const timeRemaining = timeTotal - timeElapsed; //remaining time 
+    
+    res[0] = []; //data visualization bar
+    res[1] = Math.round(timeElapsed/timeTotal * 10000 )/100  + '%'; //percent of time elapsed
+    res[2] = timeRemaining //remaining ms
+    
+    const scaler = timeTotal/36;
+    for (let i = 0; i< Math.round(timeElapsed/scaler); i++){
+        res[0].push('█');
+    }
+    for (let i = 0; i< Math.round(timeRemaining/scaler); i++){
+        res[0].push('░');
+    }
+
+    res[0] = res[0].join('');
+
+    return res.join(' ');
 }
 
-//just for today
-let stats = genStats(dayStart, dayStop);
-const workDayMs = stats[0]; //nine hours
-const elapsedMs = stats[1];
-const remMs = stats[2]; //total ms in workday minus elapsed
+const cur = new Date();
 
-console.log(stats[0], stats[1], stats[2])
+const reportText = `${genStats(dayStart, dayStop)}
+${genStats(periodStart, workStop)}
+${genStats(workStart, workStop)}
 
-let longerStats = genStats(workStart, workStop);
-const totalMsLonger = longerStats[0];
-const elapsedMsLonger = longerStats[1];
-const remMsLonger = longerStats[2];
-
-let longStats = genStats(periodStart, workStop);
-const totalMsLong = longStats[0];
-const elapsedMsLong = longStats[1];
-const remMsLong = longStats[2];
-
-const genViz = (elapsed, total, remaining) => {
-    let viz = [];
-    const scaler = total/36;
-    for (let i = 0; i< Math.round(elapsed/scaler); i++){
-        viz.push('█');
-    }
-    for (let i = 0; i< Math.round(remaining / scaler); i++){
-        viz.push('░');
-    }
-    return viz.join('');
-}
-
-const barVizArr = genViz(elapsedMs, workDayMs, remMs);
-const barVizArrLong = genViz(elapsedMsLong, totalMsLong, remMsLong);
-const barVizArrLonger = genViz(elapsedMsLonger, totalMsLonger, remMsLonger);
-
-const reportText = `${remMs}
-${remMsLong}
-${remMsLonger}
-${(Math.round((elapsedMs / workDayMs) * 10000) / 100) + "%"}
-${(Math.round((elapsedMsLong/totalMsLong) * 1000000)/10000) + '%'}
-${(Math.round((elapsedMsLonger/totalMsLonger) * 100000000)/1000000) + '%'}
-${barVizArr}
-${barVizArrLong}
-${barVizArrLonger}
-`
+${cur}
+`;
 
 console.log(reportText);
 
 //log
 const fs = require('fs');
-const logPath = `C:/Users/itdbealh/Documents/cewLogs/${dayStart.getMonth() + 1}-${dayStart.getDate()}-${dayStart.getFullYear()} ${elapsedMs}.txt`
+const logPath = `C:/Users/itdbealh/Documents/cewLogs/${cur.getMonth() + 1}-${cur.getDate()}-${cur.getFullYear()} ${Date.now()}.txt`
 
 fs.writeFile(logPath, reportText, function(err) {
     if(err) {
         return console.log(err);
     }
-    console.log("logged successfully");
+    console.log("logged successfully\n");
 }); 
